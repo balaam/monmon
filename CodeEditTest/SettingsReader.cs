46219a4ad7881713b2241f06c3a0b5319d1ac2dc
balaam <balaam@8a859188-c26f-11de-afc4-699a8c260df9>
2009-11-13 21:39:57 +0000

    Basic support for autocomplete Users can generate an autocomplete file for their Lua interfaces.

---
+++ CodeEditTest/SettingsReader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace MonMon
+{
+    public class SettingsReader
+    {
+        public SettingsReader()
+        {
+        }
+
+        public Dictionary<string, List<string>> Read()
+        {
+            string filePath = "default.xml";
+            var autocompleteTable = new Dictionary<string, List<string>>();
+            if (!File.Exists(filePath))
+            {
+                return autocompleteTable;
+            }
+            
+            using (var reader = XmlReader.Create(new FileStream(filePath, FileMode.Open)))
+            {
+              
+                string currentName = "";
+
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        if (reader.Name == "parent")
+                        {
+                            reader.MoveToAttribute("name");
+                            currentName = reader.ReadContentAsString();
+                            autocompleteTable.Add(currentName, new List<string>());
+                        }
+                        else if (reader.Name == "child")
+                        {
+                            string childData = "";
+                            // Get data add it to the autocomplete table
+                            reader.Read();
+                            while (reader.Name != "child")
+                            {
+                                switch (reader.Name)
+                                {
+                                    case "name":
+                                        {
+                                            childData = reader.ReadElementContentAsString();
+                                        } break;
+                                    default:
+                                        {
+                                            break;
+                                        }
+                                }
+                                reader.Read();
+                            }
+                            autocompleteTable[currentName].Add(childData);
+
+                        }
+                    }
+                    else
+                    {
+                        if (reader.Name == "parent")
+                        {
+                            currentName = "";
+                        }
+                    }
+                }
+            }
+
+            return autocompleteTable;
+
+           
+        }
+
+
+    }
+}
